7cfe8d5d5b2dc0b2fbc352ce5ec0dc5af5a75a73
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-09-06 12:08:54 +0000

    Add footer with social app links

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-maireadkelly-kellycooks-24rjd1u5k3y.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-maireadkelly-kellycooks-24rjd1u5k3y.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+   
     
     
     #APPS
