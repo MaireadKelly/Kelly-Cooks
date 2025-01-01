421d372bbdbf6fb4dc3ad5e86f4feab59693efaa
Máiréad O'Shea <mairead.kelly26@gmail.com>
2025-01-01 23:17:29 +0000

    CRUD functionality fix

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from djrichtextfield.models import RichTextField
 from django_resized import ResizedImageField
+from django.urls import reverse
 
 
 class Recipe(models.Model):
@@ -16,20 +17,20 @@ class Recipe(models.Model):
     )
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=False)  # Simplified this field
-    ingredients = models.TextField(null=False, blank=False)  # TextField for rich text support
-    instructions = RichTextField(max_length=10000, null=False, blank=False)  # TextField for rich text support
-    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set the timestamp on creation
-    updated_at = models.DateTimeField(auto_now=True)  # Automatically update timestamp on modification
+    ingredients = models.TextField(
+        null=False, blank=False
+    )  # TextField for rich text support
+    instructions = RichTextField(
+        max_length=10000, null=False, blank=False
+    )  # TextField for rich text support
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )  # Automatically set the timestamp on creation
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )  # Automatically update timestamp on modification
     image = CloudinaryField("image", blank=False, null=False)
 
-    # image = ResizedImageField(
-    #    size=[400, None],
-    #    quality=75,
-    #    upload_to="recipes/",
-    #    force_format="WEBP",
-    #    blank=False,
-    #    null=False,
-    # )
     image_alt = models.CharField(max_length=100, null=False, blank=False)
 
     class Meta:
@@ -37,6 +38,12 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        """
+        Returns the URL for the detail view of a recipe instance.
+        """
+        return reverse("recipe_detail", kwargs={"pk": self.pk})
 
 
 # class Follow(models.Model):
@@ -57,7 +64,10 @@ class Favorite(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ("user", "recipe")
+
+        constraints = [
+            models.UniqueConstraint(fields=["user", "recipe"], name="unique_user_recipe")
+        ]
 
         def __str__(self):
             return f"{self.user} favorited {self.recipe}"
@@ -70,5 +80,5 @@ class Review(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __set__(self):
+    def __str__(self):
         return f"Review by {self.user} on {self.recipe}"
